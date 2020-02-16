007652ffb8f9cf5d606aabcfb6026715d2610aad
theMindStone <nelson.a.caudill@gmail.com>
2020-02-16 01:11:24 -0500

    Add scope methods to Division

--- app/models/division.rb
+++ app/models/division.rb
@@ -2,4 +2,9 @@ class Division < ApplicationRecord
   has_and_belongs_to_many :events
   has_many :users, through: :events
   has_many :tournaments, through: :events
+
+  scope :gi, -> { where(gi: true) }
+  scope :nogi, -> { where(gi: false) }
+  scope :level, ->(level) { where("skill = ?", level) }
+
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,5 @@
 class Tournament < ApplicationRecord
   has_many :events
   has_many :users, through: :events
-  has_many :division, through: :events
+  has_many :divisions, through: :events
 end
