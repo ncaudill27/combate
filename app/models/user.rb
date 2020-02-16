fcd19b2ad2c5eac138ddc06793b226dffd99cacf
theMindStone <nelson.a.caudill@gmail.com>
2020-02-16 01:56:26 -0500

    Make adjustments to registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,23 @@ class User < ApplicationRecord
   def full_name
     first_name + " " + last_name
   end
+
+  def weight_class
+    case weight
+    when 0..139.9
+      "Fly"
+    when 140..149.9
+      "Feather"
+    when 155..169.9
+      "Light"
+    when 170..184.9
+      "Welter"
+    when 185..199.9
+      "Middle"
+    when 200..214.9
+      "Heavy"
+    when weight > 215
+      "Super"
+    end
+  end
 end
