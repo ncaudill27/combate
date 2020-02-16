9b22ff926ad4b4c555c05cabcf223fff9fc46ab0
theMindStone <nelson.a.caudill@gmail.com>
2020-02-16 13:52:04 -0500

    Make changes to registration form to accept models

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,10 +2,15 @@ class EventsController < ApplicationController
 
   def new
     @tournament = Tournament.find_by_id(params[:tournament_id])
+    @event = @tournament.events.build
   end
 
   def create
     byebug
     
+  end
+
+  def events_params
+    params.require(:event)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ApplicationRecord
   belongs_to :user
   belongs_to :tournament
   has_and_belongs_to_many :divisions
+  accepts_nested_attributes_for :divisions
 end
