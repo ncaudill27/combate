14689deea64e0c89e80d79a2d360e4b2b193667e
theMindStone <nelson.a.caudill@gmail.com>
2020-02-18 13:34:25 -0500

    Alter Events#new form

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
-  resources :users, except: [:new, :index]
+  get '/users/:user_id/bookmarks', to: 'events#index', as: 'bookmarks'
+  resources :users, except: [:new, :index] do
+    resources :events, only: [:index]
+  end
   resources :tournaments do
     resources :events, only: [:new, :create]
-    resources :users, only: [:index]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   match '/auth/:provider/callback', to: 'sessions#auth_create', via: [:get, :post]
