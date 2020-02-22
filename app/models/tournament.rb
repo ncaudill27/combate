6d785685bdf40e304960cf65a18b43e6f5543ed5
ncaudill27 <nelson.a.caudill@gmail.com>
2020-02-21 23:02:21 -0500

    Add tournament#output_date and seed file

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,4 +1,8 @@
 class Tournament < ApplicationRecord
   has_many :events
   has_many :users, through: :events
+
+  def output_date
+    date.strftime('%m/%d/%Y')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Team.create(affiliation:"Gracie Barra", location: "Miami, FL")
+Team.create(affiliation:"Alliance", location: "Atlanta, GA")
+Team.create(affiliation:"Gracie Academy", location: "Savannah, GA")
+Team.create(affiliation:"Checkmat", location: "San Diego, CA")
+
+Tournament.create(organization: "Newbreed", date: "20200618 09:00:00 AM", registration_open: "20200318 00:00:00 AM", registration_close: "20200518 00:00:00 AM")
+Tournament.create(organization: "NAGA", date: "20200618 09:00:00 AM", registration_open: "20200318 00:00:00 AM", registration_close: "20200518 00:00:00 AM")
+Tournament.create(organization: "IBJJF", date: "20200618 09:00:00 AM", registration_open: "20200318 00:00:00 AM", registration_close: "20200518 00:00:00 AM")
